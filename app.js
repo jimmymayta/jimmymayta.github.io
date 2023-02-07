20a6915a705a376ddd6a703a56eb82fae9c99448
Jimmy Mayta <jimmyymaytaj@gmail.com>
2023-02-07 15:19:02 -0400

    nuevos cambios

--- app.js
+++ app.js
@@ -60,10 +60,10 @@ function efectoHabilidades(){
             pintarBarra(html, 16, 0, intervalHtml);
         },100);
         const intervalJavascript = setInterval(function(){
-            pintarBarra(javascript, 11, 1, intervalJavascript);
+            pintarBarra(javascript, 16, 1, intervalJavascript);
         },100);
         const intervalWordpress = setInterval(function(){
-            pintarBarra(wordpress, 11, 2, intervalWordpress);
+            pintarBarra(wordpress, 15, 2, intervalWordpress);
         },100);
         const intervalPhotoshop = setInterval(function(){
             pintarBarra(photoshop, 15, 3, intervalPhotoshop);
